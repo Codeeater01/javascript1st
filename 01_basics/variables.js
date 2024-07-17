246d09ed77960ab0a9f022a3650d4014e9bb41df
Codeeater01 <148894430+Codeeater01@users.noreply.github.com>
2024-07-17 10:09:45 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+/* threre are threee types of variables let,const,var*/
+let accountId='Hello'
+//by using const we cant change the value of that variable which is declared by the const 
+const birthYear=2004
+//var is outdated and we dont use in industry Because of scope ,any one can manipulate the var variable 
+var age=19
+
+//to print all the above variables at same time using console we use console.table([])
+console.table([accountId],[birthYear],[age])
